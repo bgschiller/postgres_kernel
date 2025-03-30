d5ee87751e015828f2606612b3d8629596d01998
fedor <kobfedsur@gmail.com>
2025-03-30 20:52:42 +0300

    add kernel registre configuration

--- hatch_build.py
+++ hatch_build.py
@@ -1,7 +1,18 @@
+import shutil
+from pathlib import Path
+from ipykernel.kernelspec import make_ipkernel_cmd, write_kernel_spec
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 
 class CustomHook(BuildHookInterface):
 
     def initialize(self, version, build_data):
-        print("Message from the custom build hook")
+        dest = Path(__file__).parent.resolve() / "data_kernelspec"
+        overrides = {
+            "argv": make_ipkernel_cmd(mod='postgres_kernel')
+        }
+
+        if dest.exists():
+            shutil.rmtree(dest)
+
+        write_kernel_spec(dest, overrides=overrides)
